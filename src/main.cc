7cd34d4d30538e8acb23019e7e60c710b32d942c
Karol Baryła <lorak.mmk@gmail.com>
2021-03-12 00:53:13 +0100

    - Added command line parsing and help to executable

--- src/main.cc
+++ src/main.cc
@@ -4,28 +4,52 @@ namespace po = boost::program_options;
 #include <iostream>
 #include <string>
 
+#include <fmt/format.h>
 #include <session.hh>
 
 struct options {
     std::string host{};
     uint16_t port{};
+    bool is_worker;
+    bool is_init;
 };
 
+template<typename ...T>
+void exactly_one_of(const boost::program_options::variables_map & vm,
+                         const T &...op)
+{
+    const std::vector<std::string> args = { op... };
+    if(std::count_if(args.begin(), args.end(), [&](const std::string& s){ return vm.count(s); }) != 1)
+    {
+        throw std::logic_error(std::string("Need exactly one of mutually exclusive options"));
+    }
+}
 
-void parse_arguments(int ac, char *av[], options *options) {
-    try {
-        po::options_description desc("Usage");
-        desc.add_options()
+void parse_arguments(int ac, char *av[], options &options) {
+    po::options_description desc(fmt::format("Usage: {} [--init/--worker] [options]", av[0]));
+    po::options_description opt("Options");
+    opt.add_options()
             ("help", "Show program help")
-            ("host,H", po::value<std::string>(&options->host)->required(), "Address on which Scylla can be reached")
-            ("port,P", po::value<uint16_t>(&options->port)->required(), "port number on which Scylla can be reached");
-
+            ("init", "Initialize Scylla keyspace and tables")
+            ("worker", "Connect to Scylla and process incoming requests")
+            ("host,H", po::value<std::string>(&options.host)->required(), "Address on which Scylla can be reached")
+            ("port,P", po::value<uint16_t>(&options.port)->required(), "port number on which Scylla can be reached");
+    desc.add(opt);
+    try {
+        auto parsed = po::command_line_parser(ac, av)
+                .options(desc)
+                .run();
         po::variables_map vm;
-        po::store(po::parse_command_line(ac, av, desc), vm);
-        if (vm.count("help")) {
+        po::store(parsed, vm);
+        if (vm.count("help") || ac == 1) {
             std::cout << desc << "\n";
             std::exit(0);
         }
+
+        exactly_one_of(vm, "init", "worker");
+        if(vm.count("init")) options.is_init = true;
+        if(vm.count("worker")) options.is_worker = true;
+
         po::notify(vm);
     } catch (std::exception &e) {
         std::cerr << "error: " << e.what() << "\n";
@@ -36,22 +60,10 @@ void parse_arguments(int ac, char *av[], options *options) {
     }
 }
 
-/* Use this program once to initialize the database */
-int main(int argc, char **argv) {
-    struct options op;
-    parse_arguments(argc, argv, &op);
+void init(const struct options& op) {
+    std::cerr << "Connecting to " << op.host << ":" << op.port << "..." << std::endl;
 
-    if (argc > 3) {
-        std::cout << "Usage: " << argv[0] << " [IP address] [port]" << std::endl;
-        exit(0);
-    }
-
-    std::string ip_address = argc > 1 ? argv[1] : "172.17.0.2"; // docker address = default
-    std::string port = argc > 2 ? argv[2] : "9042";
-
-    std::cerr << "Connecting to " << ip_address << ":" << port << "..." << std::endl;
-
-    scmd::session session{ip_address, port};
+    scmd::session session{op.host, std::to_string(op.port)};
     std::string init_namespace = "CREATE KEYSPACE IF NOT EXISTS blas WITH REPLICATION = {"
                                  "  'class' : 'SimpleStrategy',"
                                  "  'replication_factor' : 1"
@@ -66,6 +78,25 @@ int main(int argc, char **argv) {
     session.execute(init_sets);
 
     std::cerr << "Database initialized succesfully!" << std::endl;
+}
+
+void worker(const struct options& op) {
+    std::cout << "Go to worker loop\n";
+}
+
+/* Use this program once to initialize the database */
+int main(int argc, char **argv) {
+    struct options op;
+    parse_arguments(argc, argv, op);
+    if(op.is_init) {
+        init(op);
+    } else if (op.is_worker) {
+        worker(op);
+    } else {
+        // This code should be unreachable
+        throw std::logic_error("How did we get here?");
+    }
+
     return 0;
 }
 
