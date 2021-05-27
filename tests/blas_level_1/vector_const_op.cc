b6d4ef7e49a03ec240817b86955048413b6b2eca
Robert M <robertos1727@gmail.com>
2021-05-27 22:50:27 +0200

    Update vector_const_op.cc

--- tests/blas_level_1/vector_const_op.cc
+++ tests/blas_level_1/vector_const_op.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <boost/test/unit_test.hpp>
 
 #include "../test_utils.hh"
@@ -24,22 +25,52 @@ BOOST_FIXTURE_TEST_CASE(vector_dot_float, vector_fixture)
 
     // Then the dot product is correctly calculated and equal to sum.
     BOOST_CHECK(abs(sum - res) < scylla_blas::epsilon);
-//    /* DOT */
-//    std::cout << "sdot(float_A, float_B) = " << scheduler->sdot(*float_A, *float_A2) << std::endl;
-//    std::cout << "ddot(double_A, double_B) = " << scheduler->ddot(*double_A, *double_A2) << std::endl;
-//    std::cout << "sdsdot(0.5, float_A, float_B) = " << scheduler->sdsdot(0.5, *float_A, *float_A2) << std::endl;
-//    std::cout << "dsdot(float_A, float_B) = " << scheduler->dsdot(*float_A, *float_A2) << std::endl;
-//
-//    /* One-vector-ops */
-//    std::cout << "Norm2 of float_A: " << scheduler->snrm2(*float_A) << std::endl;
-//    std::cout << "Sum of modules of float_A: " << scheduler->sasum(*float_A) << std::endl;
-//    std::cout << "Max value index of float_A: " << scheduler->isamax(*float_A) << std::endl;
-//
-//    std::cout << "Norm2 of double_A: " << scheduler->dnrm2(*double_A) << std::endl;
-//    std::cout << "Sum of modules of double_A: " << scheduler->dasum(*double_A) << std::endl;
-//    std::cout << "Max value index of double_A: " << scheduler->idamax(*double_A) << std::endl;
 }
 
+BOOST_FIXTURE_TEST_CASE(vector_sdsdot_float, vector_fixture)
+{
+    // Given two vector of five values.
+    std::vector<float> values1 = {4.234f, 3214.4243f, 290342.0f, 0.0f, -1.0f};
+    std::vector<float> values2 = {3.0f, 392.9001f, 0.005f, 5.0f, 29844.05325811f};
+    auto vector1 = getScyllaVectorOf(test_const::float_vector_1_id, values1);
+    auto vector2 = getScyllaVectorOf(test_const::float_vector_2_id, values2);
+
+    // When performing dot product of double precision plus value
+    // of these two vectors.
+    float res = scheduler->sdsdot(0.5f, *vector1, *vector2);
+
+    float sum = 0.5f;
+    for (int i = 0; i < values1.size(); i++) {
+        sum += values1[i] * values2[i];
+    }
+    std::cout << std::setprecision(20) << sum << "=sum\n";
+    std::cout << std::setprecision(20) << res << "=res\n";
+
+    // Then the dot product is correctly calculated and equal to sum + value.
+    BOOST_CHECK(abs(sum - res) < scylla_blas::epsilon);
+}
+
+BOOST_FIXTURE_TEST_CASE(vector_dsdot_float, vector_fixture)
+{
+    // Given two vector of five values.
+    std::vector<float> values1 = {4.234f, 3214.4243f, 290342.0f, 0.0f, -1.0f};
+    std::vector<float> values2 = {3.0f, 392.9001f, 0.005f, 5.0f, 29844.05325811f};
+    auto vector1 = getScyllaVectorOf(test_const::float_vector_1_id, values1);
+    auto vector2 = getScyllaVectorOf(test_const::float_vector_2_id, values2);
+
+    // When performing dot product with double precision of these two vectors.
+    double res = scheduler->dsdot(*vector1, *vector2);
+
+    double sum = 0.0f;
+    for (int i = 0; i < values1.size(); i++) {
+        sum += values1[i] * values2[i];
+    }
+    std::cout << std::setprecision(20) << sum << "=sum\n";
+    std::cout << std::setprecision(20) << res << "=res\n";
+
+    // Then the dot product is correctly calculated and equal to sum.
+    BOOST_CHECK(abs(sum - res) < scylla_blas::epsilon);
+}
 
 BOOST_FIXTURE_TEST_CASE(vector_dot_double, vector_fixture)
 {
@@ -63,44 +94,129 @@ BOOST_FIXTURE_TEST_CASE(vector_dot_double, vector_fixture)
     BOOST_CHECK(abs(sum - res) < scylla_blas::epsilon);
 }
 
+
 BOOST_FIXTURE_TEST_CASE(vector_norm_float, vector_fixture)
 {
-    // Given two vector of five values.
+    // Given vector of some values.
     std::vector<float> values1 = {0.00494931f, 0.119193f, 0.927604f, 0.354004f};
     auto vector1 = getScyllaVectorOf(test_const::float_vector_1_id, values1);
 
-    // When performing dot product of these two vectors.
+    // When performing vector euclidean norm.
     float res = scheduler->snrm2(*vector1);
 
     float nrm = 0;
-    for (int i = 0; i < values1.size(); i++) {
-        nrm += values1[i] * values1[i];
+    for (float v : values1) {
+        nrm += v * v;
     }
-    nrm = sqrt(nrm);
+    nrm = std::sqrt(nrm);
     std::cout << std::setprecision(20) << nrm << "=sum\n";
     std::cout << std::setprecision(20) << res << "=res\n";
 
-    // Then the dot product is correctly calculated and equal to sum.
+    // Then the norm is correctly calculated and equal to nrm.
     BOOST_CHECK(abs(nrm - res) < scylla_blas::epsilon);
+}
+
+BOOST_FIXTURE_TEST_CASE(vector_sasum_float, vector_fixture)
+{
+    // Given vector of some values.
+    std::vector<float> values1 = {0.00494931f, 0.119193f, -0.927604f, 0.354004f};
+    auto vector1 = getScyllaVectorOf(test_const::float_vector_1_id, values1);
+
+    // When performing sum of absolute values in this vector.
+    float res = scheduler->sasum(*vector1);
+
+    float abs_sum = 0;
+    for (float v : values1) {
+        abs_sum += abs(v);
+    }
+    std::cout << std::setprecision(20) << abs_sum << "=sum\n";
+    std::cout << std::setprecision(20) << res << "=res\n";
+
+    // Then it is correctly calculated and equal to sum.
+    BOOST_CHECK(abs(abs_sum - res) < scylla_blas::epsilon);
+}
+
+BOOST_FIXTURE_TEST_CASE(vector_isamax_float, vector_fixture)
+{
+    // Given vector of some values.
+    std::vector<float> values1 = {0.00494931f, 0.119193f, -0.927604f, 0.354004f};
+    auto vector1 = getScyllaVectorOf(test_const::float_vector_1_id, values1);
+
+    // When performing max fetch on the abs values in this vector.
+    scylla_blas::index_type res = scheduler->isamax(*vector1);
+
+    scylla_blas::index_type max_index = 0;
+    for (scylla_blas::index_type i = 1; i < values1.size(); i++) {
+        if (abs(values1[i]) > abs(values1[max_index])) {
+            max_index = i;
+        }
+    }
+    std::cout << std::setprecision(20) << values1[max_index] << "=sum\n";
+    std::cout << res << "=res index\n";
+
+    // Then the found index corresponds to the index of largest absolute value's index.
+    BOOST_CHECK(max_index + 1 == res);
 }
 
 BOOST_FIXTURE_TEST_CASE(vector_norm_double, vector_fixture)
 {
-    // Given two vector of five values.
+    // Given vector of some values.
     std::vector<double> values1 = {0.00494931, 0.119193, 0.927604, 0.354004};
     auto vector1 = getScyllaVectorOf(test_const::double_vector_1_id, values1);
 
-    // When performing dot product of these two vectors.
+    // When performing vector euclidean norm.
     double res = scheduler->dnrm2(*vector1);
 
     double nrm = 0;
-    for (int i = 0; i < values1.size(); i++) {
-        nrm += values1[i] * values1[i];
+    for (double i : values1) {
+        nrm += i * i;
     }
     nrm = sqrt(nrm);
     std::cout << std::setprecision(20) << nrm << "=sum\n";
     std::cout << std::setprecision(20) << res << "=res\n";
 
-    // Then the dot product is correctly calculated and equal to sum.
+    // Then the norm is correctly calculated and equal to nrm.
     BOOST_CHECK(abs(nrm - res) < scylla_blas::epsilon);
+}
+
+BOOST_FIXTURE_TEST_CASE(vector_dasum_double, vector_fixture)
+{
+    // Given vector of some values.
+    std::vector<double> values1 = {0.00494931, 0.119193, -0.927604, 0.354004};
+    auto vector1 = getScyllaVectorOf(test_const::double_vector_1_id, values1);
+
+    // When performing sum of absolute values in this vector.
+    double res = scheduler->dasum(*vector1);
+
+    double abs_sum = 0;
+    for (double v : values1) {
+        abs_sum += abs(v);
+    }
+    std::cout << std::setprecision(20) << abs_sum << "=sum\n";
+    std::cout << std::setprecision(20) << res << "=res\n";
+
+    // Then it is correctly calculated and equal to sum.
+    BOOST_CHECK(abs(abs_sum - res) < scylla_blas::epsilon);
+}
+
+BOOST_FIXTURE_TEST_CASE(vector_idamax_double, vector_fixture)
+{
+    // Given vector of some values.
+    std::vector<double> values1 = {0.00494931, 0.119193, -0.927604, 0.354004};
+    auto vector1 = getScyllaVectorOf(test_const::double_vector_1_id, values1);
+
+    // When performing max fetch on the abs values in this vector.
+    scylla_blas::index_type res = scheduler->idamax(*vector1);
+
+    scylla_blas::index_type max_index = 0;
+    for (scylla_blas::index_type i = 1; i < values1.size(); i++) {
+        if (abs(values1[i]) > abs(values1[max_index])) {
+            max_index = i;
+        }
+    }
+    std::cout << std::setprecision(20) << values1[max_index] << "=sum\n";
+    std::cout << res << "=res index\n";
+
+    // Then the found index corresponds to the index of largest absolute value's index.
+    BOOST_CHECK(max_index + 1 == res);
 }
