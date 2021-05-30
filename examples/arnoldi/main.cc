5a4ad68e2c2cd1b353dc3719f12599ab01217254
Robert M <robertos1727@gmail.com>
2021-05-30 17:10:16 +0200

    refactor

--- examples/arnoldi/main.cc
+++ examples/arnoldi/main.cc
@@ -12,35 +12,48 @@
 #include <iomanip>
 
 template<class T>
-void print_matrix(const scylla_blas::matrix<T> &matrix) {
-    auto default_precision = std::cout.precision();
+void print_matrix_octave(const scylla_blas::matrix<T> &matrix);
 
-    std::cout << std::setprecision(4);
-    std::cout << "Matrix " << matrix.id << ": " << std::endl;
 
-    /* Show column numbering */
-    std::cout << " \\ \t";
-    for (scylla_blas::index_type j = 1; j <= matrix.column_count; j++)
-        std::cout << std::setw(6) << j << " ";
-    std::cout << std::endl;
+template <class T>
+void load_matrix_from_generator(const std::shared_ptr<scmd::session> &session,
+                                matrix_value_generator<T> &gen,
+                                scylla_blas::matrix<T> &matrix);
 
-    for (scylla_blas::index_type i = 1; i <= matrix.row_count; i++) {
-        std::cout  << i << " ->\t";
-        auto vec = matrix.get_row(i);
-        auto it = vec.begin();
-        for (scylla_blas::index_type j = 1; j <= matrix.column_count; j++) {
-            if (it != vec.end() && it->index == j) {
-                std::cout << std::setw(6) << it->value << " ";
-                it++;
-            } else {
-                std::cout << std::setw(6) << 0 << " ";
-            }
-        }
-        std::cout << std::endl;
+template<class T>
+void init_matrix(std::shared_ptr<scmd::session> session,
+                 std::shared_ptr<scylla_blas::matrix<T>>& matrix_ptr,
+                 scylla_blas::index_type w,
+                 scylla_blas::index_type h,
+                 int64_t id,
+                 std::shared_ptr<value_factory<T>> value_factory = nullptr);
+
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        throw std::runtime_error("You need to specify ip in the command line: " + std::string(argv[0]) + " scylla_ip [scylla_port] [m] [n]");
+    }
+    scylla_blas::index_type m = 20, n = 18;
+    std::string scylla_ip = argv[1];
+    std::string scylla_port = argc > 2 ? argv[2] : std::to_string(SCYLLA_DEFAULT_PORT);
+    m = argc > 4 ? std::stoi(argv[3]) : 20;
+    n = argc > 4 ? std::stoi(argv[4]) : 18;
+    if (n > m) {
+        throw std::runtime_error("n cannot be greater than m");
     }
 
-    std::cout << std::endl;
-    std::cout << std::setprecision(default_precision);
+    auto session = std::make_shared<scmd::session>(scylla_ip, scylla_port);
+
+    arnoldi::containers c = arnoldi::containers<float>(session, 123456, m, n);
+    std::shared_ptr<value_factory<float>> f =
+            std::make_shared<random_value_factory<float>>(0, 9, 142);
+    init_matrix<float>(session, c.A, m, m, c.A_id, f);
+    c.b->update_value(1, 1.0f);
+
+    auto arnoldi_iteration = arnoldi(session);
+    print_matrix_octave(*c.A);
+    arnoldi_iteration.compute(c.A, c.b, n, c.h, c.Q, c.v, c.q, c.t);
+    print_matrix_octave(*c.Q);
+    print_matrix_octave(*c.h);
 }
 
 template<class T>
@@ -70,9 +83,8 @@ void print_matrix_octave(const scylla_blas::matrix<T> &matrix) {
     std::cout << std::setprecision(default_precision);
 }
 
-template <class T>
-void load_matrix_from_generator(const std::shared_ptr<scmd::session> &session,
-                                matrix_value_generator<T> &gen,
+template<class T>
+void load_matrix_from_generator(const std::shared_ptr<scmd::session> &session, matrix_value_generator<T> &gen,
                                 scylla_blas::matrix<T> &matrix) {
     scylla_blas::vector_segment<T> next_row;
     scylla_blas::matrix_value<T> prev_val (-1, -1, 0);
@@ -97,62 +109,17 @@ void load_matrix_from_generator(const std::shared_ptr<scmd::session> &session,
 }
 
 template<class T>
-void init_vector(std::shared_ptr<scmd::session> session,
-                 std::shared_ptr<scylla_blas::vector<T>> &vector_ptr,
-                 scylla_blas::index_type len,
-                 int64_t id,
-                 std::shared_ptr<value_factory<T>> value_factory = nullptr) {
-    scylla_blas::vector<T>::clear(session, id);
-    vector_ptr = std::make_shared<scylla_blas::vector<T>>(session, id);
-
-    if (value_factory != nullptr) {
-        std::vector<scylla_blas::vector_value<T>> values;
-
-        for (scylla_blas::index_type i = 1; i <= len; i++)
-            values.emplace_back(i, value_factory->next());
-
-        vector_ptr->update_values(values);
-    }
-}
-
-template<class T>
 void init_matrix(std::shared_ptr<scmd::session> session,
-                 std::shared_ptr<scylla_blas::matrix<T>>& matrix_ptr,
+                 std::shared_ptr<scylla_blas::matrix<T>> &matrix_ptr,
                  scylla_blas::index_type w,
                  scylla_blas::index_type h,
                  int64_t id,
-                 std::shared_ptr<value_factory<T>> value_factory = nullptr) {
-//    matrix_ptr = std::make_shared<scylla_blas::matrix<T>>(session, id);
+                 std::shared_ptr<value_factory <T>> value_factory) {
+    //    matrix_ptr = std::make_shared<scylla_blas::matrix<T>>(session, id);
     matrix_ptr->clear_all();
 
     if (value_factory != nullptr) {
         sparse_matrix_value_generator<T> gen(w, h, w * h / 5, id, value_factory);
         load_matrix_from_generator(session, gen, *matrix_ptr);
     }
-}
-
-int main(int argc, char **argv) {
-    if (argc <= 1) {
-        throw std::runtime_error("You need to specify ip in the command line: " + std::string(argv[0]) + " scylla_ip [scylla_port]");
-    }
-    std::string scylla_ip = argv[1];
-    std::string scylla_port = argc > 2 ? argv[2] : std::to_string(SCYLLA_DEFAULT_PORT);
-
-    auto session = std::make_shared<scmd::session>(scylla_ip, scylla_port);
-
-    scylla_blas::index_type m = 10, n = 9;
-    arnoldi::containers c = arnoldi::containers<float>(session, 123456, m, n);
-    //sparse_matrix_value_generator<float> v = sparse_matrix_value_generator<float>(m, m, 300*10, 12345, random_value_factory<float>(0, 123, 123321));
-    std::shared_ptr<value_factory<float>> f =
-            std::make_shared<random_value_factory<float>>(0, 9, 142);
-    init_matrix<float>(session, c.A, m, m, c.A_id, f);
-    c.b->update_value(1, 1.0f);
-    auto arnoldi_iteration = arnoldi(session);
-    print_matrix_octave(*c.A);
-    arnoldi_iteration.compute(c.A, c.b, n, c.h, c.Q, c.v, c.q, c.t);
-    print_matrix_octave(*c.Q);
-    print_matrix_octave(*c.h);
-
-    auto test = c.Q->get_row(0);
-    if (!test.empty()) std::cout << "oops";
 }
