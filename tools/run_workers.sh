0f60e5be9e8dad4fc37a361657cf4789d11752bd
Karol Baryła <lorak.mmk@gmail.com>
2021-06-08 16:18:50 +0200

    changed script

--- tools/run_workers.sh
+++ tools/run_workers.sh
@@ -78,6 +78,6 @@ done
 screen -dmS workers bash
 echo "Starting commands instances"
 for i in $(seq 1 $instances); do
-  screen -S workers -X screen -t "worker_$i" -- gdb -ex 'run' -- $@
+  screen -S workers -X screen -t "worker_$i" -- gdb -ex 'run' --args $@
   sleep $sleep_time
 done
